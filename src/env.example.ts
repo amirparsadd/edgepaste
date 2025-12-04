21547526771885934a0fe39f588464269041f6ea
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-12-04 15:37:59 +0330

    add types for env

--- src/env.example.ts
+++ src/env.example.ts
@@ -1,11 +1,19 @@
-export const S3 = {
+export const S3: {
+  accessKey: string,
+  secretKey: string,
+  endpoint: string,
+  bucket: string
+} = {
   accessKey: "123",
   secretKey: "123",
   endpoint: "example.com",
   bucket: "bucketname"
 }
 
-export const DEV = {
+export const DEV: {
+  PORT: number,
+  HOSTNAME?: string | undefined
+} = {
   PORT: 3000,
   HOSTNAME: undefined
 }
